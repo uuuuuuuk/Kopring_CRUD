671144ad993f2c74b2758f419d109828f0aea40d
uuuuuuuk <s22034@gsm.hs.kr>
2023-05-16 22:36:32 +0900

    setting

---
+++ src/main/kotlin/exam/demodemo/DemodemoApplication.kt
@@ -0,0 +1,11 @@
+package exam.demodemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemodemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemodemoApplication>(*args)
+}
