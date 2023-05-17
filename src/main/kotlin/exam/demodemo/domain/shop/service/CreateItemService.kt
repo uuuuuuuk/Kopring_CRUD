20429150cc8426a32d167894e775ca5b5603dec3
uuuuuuuk <s22034@gsm.hs.kr>
2023-05-17 22:46:34 +0900

    :sparkles: 상품 등록 api 구현

---
+++ src/main/kotlin/exam/demodemo/domain/shop/service/CreateItemService.kt
@@ -0,0 +1,35 @@
+package exam.demodemo.domain.shop.service
+
+import exam.demodemo.domain.shop.entity.Item
+import exam.demodemo.domain.shop.presentation.data.dto.CreateItemDto
+import exam.demodemo.domain.shop.presentation.data.request.CreateItemRequest
+import exam.demodemo.domain.shop.util.ItemSaveUtil
+import exam.demodemo.global.annotation.RollbackServiceAnnotation
+import java.time.LocalDateTime
+
+@RollbackServiceAnnotation
+class CreateItemService (
+
+    private val itemSaveUtil: ItemSaveUtil
+) {
+    fun execute(createItemRequest: CreateItemRequest): Item {
+        val createItemDto: CreateItemDto = toDto(createItemRequest = createItemRequest)
+
+        return toEntity(createItemDto)
+                .let { itemSaveUtil.saveItem(item = it) }
+    }
+
+    private fun toDto(createItemRequest: CreateItemRequest): CreateItemDto =
+            CreateItemDto(
+                    title = createItemRequest.title,
+                    content = createItemRequest.content
+            )
+
+    private fun toEntity(createItemDto: CreateItemDto): Item =
+            Item(
+                    title = createItemDto.title,
+                    content = createItemDto.content,
+                    createTime = LocalDateTime.now(),
+                    editTime = LocalDateTime.now()
+            )
+}
