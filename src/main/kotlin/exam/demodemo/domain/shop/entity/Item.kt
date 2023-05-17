145025f4f6f50f0b878bbeeffe9f19f10a292cc2
uuuuuuuk <s22034@gsm.hs.kr>
2023-05-17 22:45:01 +0900

    :memo: Item 테이블 작성

---
+++ src/main/kotlin/exam/demodemo/domain/shop/entity/Item.kt
@@ -0,0 +1,32 @@
+package exam.demodemo.domain.shop.entity
+
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+import javax.persistence.*
+
+@Entity
+@Table(name = "item")
+@EntityListeners(AuditingEntityListener::class)
+class Item (
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "item_id")
+        val id: Long = 0,
+        @Column(name = "title", nullable = false)
+        var title: String,
+        @Column(name = "content", nullable = false)
+        var content: String,
+        @CreatedDate
+        var createTime: LocalDateTime,
+        @LastModifiedDate
+        var editTime: LocalDateTime
+) {
+
+        fun update(title: String, content: String) {
+                this.title = title
+                this.content = content
+                this.editTime = LocalDateTime.now()
+        }
+}
