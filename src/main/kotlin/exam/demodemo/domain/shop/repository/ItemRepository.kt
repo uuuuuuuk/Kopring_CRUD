404f9394ac453e7626893650b4f87ab39bbe97d5
uuuuuuuk <s22034@gsm.hs.kr>
2023-05-18 17:26:55 +0900

    :memo: ItemRepository 에 findAll, existsByTitle 추상 메서드 추가

--- src/main/kotlin/exam/demodemo/domain/shop/repository/ItemRepository.kt
+++ src/main/kotlin/exam/demodemo/domain/shop/repository/ItemRepository.kt
@@ -4,4 +4,6 @@ import exam.demodemo.domain.shop.entity.Item
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface ItemRepository : JpaRepository<Item, Long> {
+    override fun findAll(): List<Item>
+    fun existsByTitle(title: String): Boolean
 }
