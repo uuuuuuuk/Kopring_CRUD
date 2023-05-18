07ead65401e2105c4382e6cef9373c8b798833b3
uuuuuuuk <s22034@gsm.hs.kr>
2023-05-18 17:24:56 +0900

    :memo: CreateItemDto 에 price 추가

--- src/main/kotlin/exam/demodemo/domain/shop/presentation/data/dto/CreateItemDto.kt
+++ src/main/kotlin/exam/demodemo/domain/shop/presentation/data/dto/CreateItemDto.kt
@@ -2,5 +2,6 @@ package exam.demodemo.domain.shop.presentation.data.dto
 
 data class CreateItemDto (
         val title: String,
-        val content: String
+        val content: String,
+        val price: Int
 )
