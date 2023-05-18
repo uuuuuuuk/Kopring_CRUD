41e84a33c42db612081b033a240ec864fffd424f
uuuuuuuk <s22034@gsm.hs.kr>
2023-05-18 17:27:19 +0900

    :memo: RollbackServiceAnnotation 에 설정 추가

--- src/main/kotlin/exam/demodemo/global/annotation/RollbackServiceAnnotation.kt
+++ src/main/kotlin/exam/demodemo/global/annotation/RollbackServiceAnnotation.kt
@@ -2,7 +2,13 @@ package exam.demodemo.global.annotation
 
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.lang.annotation.ElementType
+import java.lang.annotation.Retention
+import java.lang.annotation.RetentionPolicy
+import java.lang.annotation.Target
 
 @Service
 @Transactional(rollbackFor = [Exception::class])
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
 annotation class RollbackServiceAnnotation()
