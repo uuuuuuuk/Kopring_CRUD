33be7895d46100d074b8660ed694493bfbe8433f
uuuuuuuk <s22034@gsm.hs.kr>
2023-05-19 23:14:01 +0900

    .

--- src/main/kotlin/exam/demodemo/domain/shop/presentation/data/request/CreateItemRequest.kt
+++ src/main/kotlin/exam/demodemo/domain/shop/presentation/data/request/CreateItemRequest.kt
@@ -3,6 +3,7 @@ package exam.demodemo.domain.shop.presentation.data.request
 import javax.validation.constraints.NotBlank
 
 data class CreateItemRequest (
+
         @NotBlank
         var title: String,
         @NotBlank
